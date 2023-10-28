dfad5973469668bf6827438abc9f82ff8b9112f7
Dawid7600 <dawid7600@o2.pl>
2023-10-28 22:06:17 +0200

    add hamburger menu - html,css,js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const navList = document.querySelector(".nav__list");
+const btnHamburger = document.querySelector(".btn-hamburger");
+
+btnHamburger.addEventListener("click", function (event) {
+	event.stopPropagation();
+	navList.classList.toggle("nav__list-is-active");
+});
+
+document.addEventListener("click", function (event) {
+	if (!navList.contains(event.target) && !btnHamburger.contains(event.target)) {
+		navList.classList.remove("nav__list-is-active");
+	}
+});
