2679d4bad5b5b0953f7b7573a506c2e997bb9a74
Dawid7600 <dawid7600@o2.pl>
2023-11-01 12:30:21 +0100

    add form.html with styling and script to display form on '.btn-book' button click

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const navList = document.querySelector(".nav__list");
 const btnHamburger = document.querySelector(".btn-hamburger");
+const btnBook = document.querySelector(".btn-book");
+const formContainer = document.getElementById("formContainer");
+const overlay = document.getElementById("overlay");
 
 btnHamburger.addEventListener("click", function (event) {
 	event.stopPropagation();
@@ -11,3 +14,27 @@ document.addEventListener("click", function (event) {
 		navList.classList.remove("nav__list-is-active");
 	}
 });
+
+function displayForm() {
+	overlay.style.display = "block";
+
+	fetch("form.html")
+		.then((response) => response.text())
+		.then((html) => {
+			formContainer.innerHTML = html;
+
+			const btnCloseForm = document.getElementById("btnCloseForm");
+
+			btnCloseForm.addEventListener("click", () => {
+				formContainer.innerHTML = "";
+				overlay.style.display = "none";
+			});
+		});
+
+	overlay.addEventListener("click", () => {
+		formContainer.innerHTML = "";
+		overlay.style.display = "none";
+	});
+}
+
+btnBook.addEventListener("click", displayForm);
